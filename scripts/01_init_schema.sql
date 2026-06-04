3d53bdd2e79a3227d8bfe1b5cb0fb1a925cad1c9
Zeynep Irem CAN <zeynepiremcan0238@gmail.com>
2026-06-04 20:36:08 +0300

    Create 01_init_schema.sql

---
+++ scripts/01_init_schema.sql
@@ -0,0 +1,14 @@
+/*
+====================================================
+INIT SCHEMA
+====================================================
+
+Purpose:
+Creates the schema used for analytics
+objects such as reporting queries,
+and analysis artifacts.
+
+====================================================
+*/
+
+CREATE SCHEMA IF NOT EXISTS gold_analytics;
