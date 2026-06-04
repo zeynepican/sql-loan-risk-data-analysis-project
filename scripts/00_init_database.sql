462424a5a8f38a7d851fa37c32e877e94fa597ed
Zeynep Irem CAN <zeynepiremcan0238@gmail.com>
2026-06-04 20:35:08 +0300

    Update and rename placeholder to 00_init_database.sql

---
+++ scripts/00_init_database.sql
@@ -0,0 +1,19 @@
+/*
+====================================================
+INIT DATABASE
+====================================================
+
+Purpose:
+Creates the database environment for the
+Loan Analytics project.
+
+This project serves as the analytics layer
+built on top of the Loan Analytics Data
+Warehouse and is used for EDA, advanced
+analytics, and business insight generation.
+
+====================================================
+*/
+
+CREATE DATABASE loan_analytics_db;
+
